5ab8f92e1c32a45a7375167e34ace461b4f7c0ac
Alex Hivoina <alexhivoina@gmail.com>
2020-11-25 13:20:59 +0300

    fix: bugs

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 //import AuthGuard from './auth-guard'
 import Home from '@/components/Home'
-import NotFound from '@/components/Pages/NotFound'
 
 Vue.use(Router)
 
@@ -14,15 +13,6 @@ export default new Router({
             name: 'home',
             component: Home
         },
-        {
-            path: '/404',
-            name: '404',
-            component: NotFound,
-        },
-        {
-            path: '*',
-            redirect: '/404'
-        }
     ],
     mode: 'history'
 })
